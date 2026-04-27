602caa67addf9b271bce8f3f582872772d2bcf18
basimjaveed <basim.javeed@enablenow.com>
2026-04-27 10:49:43 +0530

    feat: define API and socket connection constants for local development

--- client/constants/api.ts
+++ client/constants/api.ts
@@ -1,4 +1,4 @@
-const LOCAL_IP = '172.20.10.2'; 
+const LOCAL_IP = 'REPLACE_WITH_YOUR_IP'; // Example: '192.168.1.15'
 
 export const API_URL = `http://${LOCAL_IP}:5001/api`;
 export const SOCKET_URL = `http://${LOCAL_IP}:5001`;
